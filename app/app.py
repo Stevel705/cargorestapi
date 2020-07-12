06cfd12ad7cbb663fb7bfd8dd9d9f682b7a71490
Alexandr Meshkov <stevel705@outlook.com>
2020-07-12 17:02:12 +0200

    Checking for none and change url in info

--- app/app.py
+++ app/app.py
@@ -38,7 +38,18 @@ def declared_val():
     date_ = request.args.get('date') # дата 
     cargo_type = request.args.get('cargoType') # тип груза 
     declared_value = request.args.get('declaredValue') # объявленная стоимость
+
+    if date_ is None:
+        return "No date"
+
+    if cargo_type is None:
+        return "No cargo type"
+    
+    if declared_value is None:
+        return "No declared value"
+
     # Получаем дату и приводим к первому дню месяца, используем эту дату для получения актуальной информации в json
+    # Считаем, что дата приходит в нужном формате и нам не нужно её проверять
     date_time = datetime.strptime(date_, '%Y-%m-%d').replace(day=1).strftime("%Y-%m-%d")
     rate = get_rate(date=date_time, cargo_type=cargo_type)
     
@@ -54,7 +65,7 @@ def info_view():
     # List of routes for this API:
     output = {
         'info': 'GET /',
-        'calculate declared value': 'GET /declared-value?date=yyyy-mm-dd&cargoType=cargo_type&declaredValue=declared_value',
+        'calculate declared value': 'GET /innoseti/api/v1.0/declared-value?date=yyyy-mm-dd&cargoType=cargo_type&declaredValue=declared_value',
     }
     return jsonify(output), 200
 
